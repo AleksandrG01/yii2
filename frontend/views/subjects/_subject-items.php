4e38bb74407978d34086fa387cd01bca5f29fda7
AleksandrG01 <sa6ka200711@gmail.com>
2022-12-08 23:34:35 +0300

    +

--- frontend/views/subjects/_subject-items.php
+++ frontend/views/subjects/_subject-items.php
@@ -3,7 +3,7 @@
 	<?php foreach($subjects as $subject): ?>
 		<div class="subject-item">
 			<div class="subject-image _pic-fluid">
-				<img width="100" height="100" src="<?= Yii::getAlias('@images/' . $subject->gerb) ?>" alt="<?= $subject->subject ?>">
+				<img width="100" height="100" src="<?= $subject->gerb ? Yii::getAlias('@images/' . $subject->gerb) : Yii::getAlias('@images/plaseholder.svg') ?>" alt="<?= $subject->subject ?>">
 			</div>
 			<div class="subject-deskription">
 				<div class="subject-title"><?= $subject->subject ?></div>
