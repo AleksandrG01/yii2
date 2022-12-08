8b1b7753ad8cb017f4cb49cde70a227c8149d590
AleksandrG01 <sa6ka200711@gmail.com>
2022-12-08 23:25:09 +0300

    +

--- frontend/views/subjects/_subject-items.php
+++ frontend/views/subjects/_subject-items.php
@@ -10,21 +10,21 @@
 				<div class="subject-options">
 					<div class="subject-option-item">
 						<div class="subject-option-title">Адм. центр</div>
-						<div class="subject-option-text"><?= $subject->administrative_center ?></div>
+						<div class="subject-option-text"><?= $subject->administrative_center ? $subject->administrative_center : '-' ?></div>
 					</div>
 					<div class="subject-option-item">
 						<div class="subject-option-title">Территория</div>
-						<div class="subject-option-text"><?= number_format($subject->territory, 0, '', ',') ?></div>
+						<div class="subject-option-text"><?= number_format($subject->territory, 0, '', '.') ?></div>
 					</div>
 					<div class="subject-option-item">
 						<div class="subject-option-title">Население</div>
-						<div class="subject-option-text"><?= number_format($subject->population, 0, '', ',') ?> чел.</div>
+						<div class="subject-option-text"><?= number_format($subject->population, 0, '', '.') ?> чел.</div>
 					</div>
 				</div>
 			</div>
 			<div class="subject-okato">
 				<div class="subject-okato-title">окато</div>
-				<div class="subject-okato-text"><?= $subject->okato ?></div>
+				<div class="subject-okato-text"><?= number_format($subject->okato, 0, '', '.')  ?></div>
 			</div>
 		</div>
 	<?php endforeach ?>
